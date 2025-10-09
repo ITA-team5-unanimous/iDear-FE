1ef18d910b8f0c3023ede00480647ecc319242c8
kimminna <kmina121777@naver.com>
2025-10-09 16:27:19 +0900

    chore: Husky hooks and ESLint configuration (#1)

---
+++ commitlint.config.js
@@ -0,0 +1,35 @@
+const Configuration = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'revert',
+        'chore',
+        'security',
+        'ui',
+      ],
+    ],
+    'subject-case': [
+      2,
+      'always',
+      [
+        'sentence-case',
+        'start-case',
+        'pascal-case',
+        'lower-case',
+        'upper-case',
+        'kebab-case',
+      ],
+    ],
+  },
+};
+
+export default Configuration;
