33d804a03b62c8532e3b284dae9065a2db398d96
kimminna <kmina121777@naver.com>
2025-10-19 13:00:22 +0900

    feat: update layout and home page with initial setup details (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>setup - Home</div>;
+  return (
+    <div className='p-10 text-2xl font-extrabold text-gray-800'>
+      Nextjs + TypeScript + React + Tailwindcss + Storybook 초기 설정 완료
+    </div>
+  );
 }
