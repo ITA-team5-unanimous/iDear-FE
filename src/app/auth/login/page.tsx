277ea37dbff26a35891420c28d0fd9b04f8e3c6d
kimminna <kmina121777@naver.com>
2025-11-05 23:39:07 +0900

    feat: add login page with social login buttons (#6)

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import {SocialLoginButton} from '@/components/buttons/SocialLoginButton';
+import {OAUTH_SERVICES} from '@/constants/oauth';
+import Logo from '@/assets/logo/idear.svg';
+import IdearBear from '@/assets/onboarding/idear-bear.svg';
+
+export default function LoginPage() {
+  return (
+    <div className='flex h-screen flex-col xl:flex-row'>
+      <section className='flex items-center justify-center sm:flex-[17]'>
+        <div className='absolute top-9 left-9'>
+          <Logo />
+        </div>
+        <div className='translate-y-50 xl:translate-y-12'>
+          <IdearBear />
+        </div>
+      </section>
+      <section className='xl:border-l-primary flex flex-col items-center justify-center gap-[67px] py-[400px] md:px-[50px] lg:px-[110px] xl:border-l-2'>
+        <h1 className='text-center text-2xl leading-11 font-medium sm:text-4xl'>
+          반가워요!
+          <br /> 로그인/가입 방법을 선택해 주세요.
+        </h1>
+        <ul className='flex w-full flex-col gap-6 px-[28px]'>
+          {OAUTH_SERVICES.map((service) => (
+            <SocialLoginButton key={service.name} name={service.name} />
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
