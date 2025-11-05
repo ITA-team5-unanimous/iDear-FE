dfb4f995c8353f5de8d92f509dd48daf1ec8d95f
kimminna <kmina121777@naver.com>
2025-11-05 15:53:40 +0900

    feat(storybook): configure svg handling and add static directory for fonts (#5)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,7 +12,36 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: [],
+  staticDirs: [
+    {
+      from: '../src/app/fonts',
+      to: '../src/app/fonts',
+    },
+  ],
+  webpackFinal: async (config) => {
+    if (!config.module || !config.module.rules) {
+      return config;
+    }
+
+    config.module.rules = [
+      ...config.module.rules.map((rule) => {
+        if (!rule || rule === '...') {
+          return rule;
+        }
+
+        if (rule.test && /svg/.test(String(rule.test))) {
+          return {...rule, exclude: /\.svg$/i};
+        }
+        return rule;
+      }),
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ];
+
+    return config;
+  },
 };
 
 export default config;
