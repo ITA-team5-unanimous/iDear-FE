6a01273385fe9a2717eb16efbd7ca2961f385ab2
kimminna <kmina121777@naver.com>
2025-11-05 23:32:44 +0900

    feat: 소셜 로그인 버튼 컴포넌트, 스토리 파일 생성 (#6)

---
+++ src/components/buttons/SocialLoginButton.tsx
@@ -0,0 +1,28 @@
+import {OAUTH_SERVICES} from '@/constants/oauth';
+
+interface SocialLoginButtonProps {
+  name: string;
+}
+
+export const SocialLoginButton = ({name}: SocialLoginButtonProps) => {
+  const service = OAUTH_SERVICES.find((s) => s.name === name);
+
+  if (!service) return null;
+
+  const {icon: Icon, url, bgColor, textColor, borderColor} = service;
+
+  return (
+    <a
+      href={url}
+      className={`flex h-[60px] items-center justify-center gap-[15px] rounded-md font-medium`}
+      style={{
+        backgroundColor: bgColor,
+        border: borderColor ? `1px solid ${borderColor}` : 'none',
+        color: textColor ?? '#ffffff',
+        textDecoration: 'none',
+      }}>
+      <Icon width={24} height={24} />
+      <span className='text-[18px] sm:text-xl'>{name}로 시작하기</span>
+    </a>
+  );
+};
