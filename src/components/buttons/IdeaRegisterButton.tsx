d8267134550f6463e5074d960d8a5456da4bb1b8
juhyung <njh1019@gmail.com>
2025-11-08 17:17:40 +0900

    fix: backgroundcolor 수정(#7)

--- src/components/buttons/IdeaRegisterButton.tsx
+++ src/components/buttons/IdeaRegisterButton.tsx
@@ -9,9 +9,9 @@ export default function IdeaRegisterButton({
 }: IdeaRegisterButtonProps) {
   return (
     <button
+      aria-label='아이디어 등록하기 버튼'
       onClick={onClickRegister}
-      style={{backgroundColor: '#DB0001'}}
-      className='rounded-sm pt-3 pr-6 pb-3 pl-6 text-lg text-white'>
+      className='bg-primary rounded-sm pt-3 pr-6 pb-3 pl-6 text-lg text-white'>
       아이디어 등록하기
     </button>
   );
