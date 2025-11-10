db0e98037c22ad61e18d2327fb43e8632421c8b8
kimminna <kmina121777@naver.com>
2025-11-10 20:07:06 +0900

    feat: add register complete modal component & story (#16)

---
+++ src/stories/modal/RegisterCompleteModal.stories.tsx
@@ -0,0 +1,27 @@
+import {RegisterCompleteModal} from '@/components/common/modal/RegisterCompleteModal';
+import {ModalWrapper} from '@/components/common/wrappers/ModalWrapper';
+import {Meta, StoryObj} from '@storybook/nextjs';
+
+const meta: Meta<typeof RegisterCompleteModal> = {
+  title: 'Components/Modal/Register Complete Modal',
+  tags: ['autodocs'],
+  component: RegisterCompleteModal,
+  parameters: {
+    docs: {
+      description: {
+        component: `아이디어 등록이 완료되었음을 사용자에게 알려주는 모달입니다. `,
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof RegisterCompleteModal>;
+
+export const Default: Story = {
+  render: () => (
+    <ModalWrapper isOpen={true}>
+      <RegisterCompleteModal />
+    </ModalWrapper>
+  ),
+};
