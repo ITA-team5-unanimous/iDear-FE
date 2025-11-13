6063c54976dc87f21ec19132575d05ee87b6d466
juhyung <njh1019@gmail.com>
2025-11-14 00:07:02 +0900

    feat: 공모전 카드, 인기 공모전 mock 데이터 생성(#7)

---
+++ src/mocks/data/mockContestCard.ts
@@ -0,0 +1,90 @@
+import type Contest from '@/types/Contest';
+
+const mockContestCard: Contest[] = [
+  {
+    id: 0,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 1,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 2,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 3,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 4,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 5,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 6,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 7,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 8,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 9,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 10,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 11,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '국립부경대학교',
+    d_day: 10,
+    image: undefined,
+  },
+];
+
+export default mockContestCard;
---
+++ src/mocks/data/mockPopularContestList.ts
@@ -0,0 +1,39 @@
+import Contest from '@/types/Contest';
+
+export const mockPopularContestList: Contest[] = [
+  {
+    id: 0,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '기관 A',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 1,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '기관 A',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 2,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '기관 A',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 3,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '기관 A',
+    d_day: 10,
+    image: undefined,
+  },
+  {
+    id: 4,
+    name: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    organizer: '기관 A',
+    d_day: 10,
+    image: undefined,
+  },
+];
