b0ddda8037a5da888a3d20842d7f42491113568d
juhyung <njh1019@gmail.com>
2025-11-13 23:56:25 +0900

    fix: contest 타입 image 필드 수정(#7)

--- src/types/Contest.ts
+++ src/types/Contest.ts
@@ -1,7 +1,9 @@
+import {StaticImageData} from 'next/image';
+
 export default interface Contest {
   id: number;
   name: string;
   organizer: string;
   d_day: number;
-  image?: string;
+  image?: string | StaticImageData;
 }
