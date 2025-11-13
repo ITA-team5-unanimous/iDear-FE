572f321c276352202da9c892ac6f5ec5911df8bc
juhyung <njh1019@gmail.com>
2025-11-14 03:04:40 +0900

    chore: 공모전 카드, 인기 공모전 mock 데이터 추가(#26)

---
+++ src/mocks/data/mockContestCard.ts
@@ -0,0 +1,92 @@
+import {z} from 'zod';
+import {contestSchema} from '@/schemas/contests';
+
+type Contest = z.infer<typeof contestSchema>;
+const mockContestCard: Contest[] = [
+  {
+    id: 0,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 1,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 2,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 3,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 4,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 5,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 6,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 7,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 8,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 9,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 10,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+  {
+    id: 11,
+    title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어]',
+    hostingOrganization: '국립부경대학교',
+    d_day: 10,
+    imageUrl: undefined,
+  },
+];
+
+export default mockContestCard;
