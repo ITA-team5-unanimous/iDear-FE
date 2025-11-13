514a836c3249e91e15f271f5688599cf384eee41
juhyung <njh1019@gmail.com>
2025-11-14 02:56:00 +0900

    fix: 이미지 svg?url 수정(#26)

---
+++ src/next-images.d.ts
@@ -0,0 +1,11 @@
+declare module '*.svg?url' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default content;
+}
+s;
