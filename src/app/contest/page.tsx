cae105d8b59c2a52b6a466aacc22a90b371f5bd1
kimminna <kmina121777@naver.com>
2025-11-15 01:43:21 +0900

    chore: remove unused variable and comment (#24)

--- src/app/contest/page.tsx
+++ src/app/contest/page.tsx
@@ -9,15 +9,14 @@ import {mockContestCard} from '@/mocks/data/mockContestCard';
 import {UpFloatingButton} from '@/components/buttons/UpFloatingButton';
 
 export default function ContestMainPage() {
+  const contests = mockContestCard;
+  const [isSaved, setIsSaved] = useState<boolean>(false);
+
   const handleSearchClick = (keyword: string) => {
     console.log('검색어:', keyword);
   };
 
-  const [isSaved, setIsSaved] = useState<boolean>(false);
-
   const handleSaveClick = () => setIsSaved((prev) => !prev);
-
-  const contests = mockContestCard;
 
   const handleScrollToTop = () => {
     window.scrollTo({
@@ -28,14 +27,13 @@ export default function ContestMainPage() {
 
   return (
     <div className='relative mb-20 flex flex-col items-center'>
-      {/* 검색바 */}
       <div className='mt-9 flex'>
         <GlobalSearchBar
           onSearch={handleSearchClick}
           placeholder='다양한 공모전을 검색해보세요!'
         />
       </div>
-      {/* 인기 공모전*/}
+
       <div className='border-primary mt-9 flex h-auto w-[1400px] flex-col rounded-[4px] border-2 p-12'>
         <div className='flex flex-row items-baseline'>
           <p className='text-2xl leading-normal font-extrabold text-black'>
@@ -45,20 +43,20 @@ export default function ContestMainPage() {
             현재 가장 인기있는 공모전을 알려드릴게요!
           </p>
         </div>
-        {/* 공모전 목록 (PopularContestList) 삽입 */}
+
         <div className='mt-8 flex'>
           <PopularContestList />
         </div>
       </div>
-      {/*드롭다운*/}
+
       <div className='flex w-[1400px] flex-row justify-start pt-9'>
         <SortDropdownContainer />
-        {/* 저장 버튼*/}
+
         <div className='ml-5.5'>
           <SaveButton isOpen={isSaved} onClick={handleSaveClick} />
         </div>
       </div>
-      {/*공모전카드*/}
+
       <div className='mt-9 w-[1400px]'>
         <div className='grid grid-cols-2 gap-x-[40px] gap-y-9'>
           {contests.map((contest) => (
@@ -66,7 +64,7 @@ export default function ContestMainPage() {
           ))}
         </div>
       </div>
-      {/*UpFloatingButton*/}
+
       <div className='absolute right-10 bottom-80 z-50'>
         <UpFloatingButton onClick={handleScrollToTop} />
       </div>
