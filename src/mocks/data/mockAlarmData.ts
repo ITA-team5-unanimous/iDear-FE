016568271799cd7bc63d73d26790e0bddafa07bc
kimminna <kmina121777@naver.com>
2025-11-16 14:44:34 +0900

    chore: add mock alarm data and schema (#33)

---
+++ src/mocks/data/mockAlarmData.ts
@@ -0,0 +1,20 @@
+import {Alarm} from '@/schemas/alarm';
+
+export const mockAlarmData: Alarm[] = [
+  {
+    id: 0,
+    date: '11월 31일',
+  },
+  {
+    id: 1,
+    date: '11월 31일',
+  },
+  {
+    id: 2,
+    date: '11월 31일',
+  },
+  {
+    id: 3,
+    date: '11월 31일',
+  },
+];
