443443c366728d71e5c67b6b3628acc37fd3db5d
kimminna <kmina121777@naver.com>
2025-11-16 14:45:41 +0900

    feat: add alarm container & item component and stories (#33)

---
+++ src/components/alarm/AlarmItem.tsx
@@ -0,0 +1,24 @@
+import {ALARM_TEXT} from '@/constants/gnb-option';
+import AlarmDelete from '@/assets/alarm/alarm-delete.svg';
+
+// 알림 아이템 클릭시 해당 아이디어 기록함으로 이동
+
+interface AlarmItemProps {
+  date: string;
+  onDelete: () => void;
+}
+
+export const AlarmItem = ({date, onDelete}: AlarmItemProps) => {
+  return (
+    <li className='relative flex flex-col gap-3 rounded-[4px] bg-white p-6'>
+      <p>{date}</p>
+      <p className='min-h-[66px]'>{ALARM_TEXT}</p>
+      <button
+        className='absolute top-3 right-3'
+        aria-label='알림 삭제 버튼'
+        onClick={onDelete}>
+        <AlarmDelete />
+      </button>
+    </li>
+  );
+};
