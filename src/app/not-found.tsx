6c1d9b67860db250ea439d25b7bf326f1bbbd44d
kimminna <kmina121777@naver.com>
2025-11-19 16:26:01 +0900

    style: 중복되는 스타일링 통합 (#42)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,16 +6,10 @@ export default function NotFound() {
   return (
     <section className='flex min-h-screen min-w-full flex-col items-center justify-start gap-9 pt-80'>
       <h1 className='text-primary text-[32px] font-bold'>404 ERROR</h1>
-      <div className='text-center leading-snug'>
-        <p className='text-[32px] font-medium text-black'>
-          페이지를 찾을 수 없습니다.
-        </p>
-        <p className='text-[32px] font-medium text-black'>
-          존재하지 않는 주소를 입력하셨거나,
-        </p>
-        <p className='text-[32px] font-medium text-black'>
-          요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
-        </p>
+      <div className='text-center text-[32px] leading-snug font-medium text-black'>
+        <p>페이지를 찾을 수 없습니다.</p>
+        <p>존재하지 않는 주소를 입력하셨거나,</p>
+        <p>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.</p>
       </div>
 
       <Link href={ROUTES.MAIN}>
