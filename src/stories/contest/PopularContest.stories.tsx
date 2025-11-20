9b626b3c8565a1347fb19a50f75ad11f6a9de7ec
juhyung <njh1019@gmail.com>
2025-11-20 17:09:36 +0900

    fix: 인기공모전 storybook nextjs 및 id 속성 추가(#30)

--- src/stories/contest/PopularContest.stories.tsx
+++ src/stories/contest/PopularContest.stories.tsx
@@ -11,8 +11,8 @@ const meta: Meta<typeof PopularContest> = {
       description: {
         component: `인기 공모전 카드를 열마다 5개씩 보여주는 컴포넌트입니다.`,
       },
-      nextjs: {appDirectory: true},
     },
+    nextjs: {appDirectory: true},
   },
   argTypes: {
     title: {
@@ -31,6 +31,7 @@ type Story = StoryObj<typeof PopularContest>;
 
 export const Default: Story = {
   args: {
+    id: 0,
     title: '2025년 [부산시민 모두를 위한 디지털 금융포용 정책 아이디어] 공모전',
     imageUrl:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMKRY63t_07oVVNkww1iMbnsyiJcr5qzWhhg&s',
