e7dab9605149eb4ba19f38c81ed750c2333f203b
kimminna <kmina121777@naver.com>
2025-11-22 19:42:10 +0900

    refactor: move client-side rendering to component level (#50)

--- src/components/dropdown/SortDropdownContainer.tsx
+++ src/components/dropdown/SortDropdownContainer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {useState} from 'react';
 import {SortDropdown} from '@/components/dropdown/SortDropdown';
 import {DropdownOption} from '@/components/dropdown/DropdownOption';
