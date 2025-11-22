8269101c361b321f1b44171db32a9d16acd1234f
kimminna <kmina121777@naver.com>
2025-11-22 19:37:47 +0900

    refactor: extract scroll to top custom hook (#50)

--- src/components/buttons/UpFloatingButton.tsx
+++ src/components/buttons/UpFloatingButton.tsx
@@ -1,14 +1,15 @@
+'use client';
+
 import ChevronUpIcon from '@/assets/chevrons/chevron-up-white.svg';
+import {useScrollToTop} from '@/hooks/ui/useScrollToTop';
 
-interface UpFloatingButtonProps {
-  onClick?: () => void;
-}
+export const UpFloatingButton = () => {
+  const scrollToTop = useScrollToTop();
 
-export const UpFloatingButton = ({onClick}: UpFloatingButtonProps) => {
   return (
     <button
       type='button'
-      onClick={onClick}
+      onClick={scrollToTop}
       aria-label='페이지 상단으로 이동'
       className='bg-primary flex h-[72px] w-[72px] flex-shrink-0 items-center justify-center rounded-full transition-transform duration-100 hover:scale-105 active:scale-95'>
       <ChevronUpIcon />
---
+++ src/hooks/ui/useScrollToTop.ts
@@ -0,0 +1,7 @@
+import {useCallback} from 'react';
+
+export const useScrollToTop = () => {
+  return useCallback(() => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }, []);
+};
