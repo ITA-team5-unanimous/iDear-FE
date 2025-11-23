0c754c500ded5cf4acab9a01934e1650a7dae2a0
kimminna <kmina121777@naver.com>
2025-11-23 17:32:44 +0900

    feat: add route constants for support page (#52)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,4 +4,7 @@ export const ROUTES = {
   CONTEST: '/contest',
   IDEA: '/idea',
   CONTEST_SEARCH: '/contest/search',
+  SUPPORT: '/support',
+  SUPPORT_INQUIRY: '/support/inquiry',
+  SUPPORT_INQUIRY_NEW: '/support/inquiry/new',
 };
