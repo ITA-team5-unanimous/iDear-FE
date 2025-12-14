8d217e5b633ddc900bc2f531ecec14699dad0c1e
kimminna <kmina121777@naver.com>
2025-12-15 00:08:48 +0900

    feat: add contest api function & queries & schema (#62)

---
+++ src/hooks/queries/useContest.ts
@@ -0,0 +1,91 @@
+import {
+  deleteBookMarkedContest,
+  getBookmarkedContestList,
+  getContestDetail,
+  getContestList,
+  getPopularContest,
+  postBookMarkedContest,
+} from '@/services/api/contest/contestApi';
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+
+type SortType = 'latest' | 'popular' | 'deadline';
+
+export const useContestList = (
+  mode: 'all' | 'bookmarked',
+  sortBy: SortType
+) => {
+  return useInfiniteQuery({
+    queryKey: ['contestList', mode, sortBy],
+    queryFn: async ({pageParam = 0}) => {
+      if (mode === 'bookmarked') {
+        const response = await getBookmarkedContestList({
+          page: pageParam,
+          size: 10,
+          sortBy,
+        });
+        return response.data;
+      }
+
+      const response = await getContestList({
+        page: pageParam,
+        size: 10,
+        sortBy,
+      });
+      return response.data;
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      const nextPage = allPages.length;
+      return nextPage < lastPage.totalPages ? nextPage : undefined;
+    },
+    staleTime: 1000 * 60 * 5,
+  });
+};
+
+export const usePopularContests = () =>
+  useQuery({
+    queryKey: ['popularContests'],
+    queryFn: getPopularContest,
+    staleTime: 1000 * 60 * 5,
+  });
+
+export const useContestDetail = (contestId: number) => {
+  return useQuery({
+    queryKey: ['contestDetail', contestId],
+    queryFn: async () => {
+      const response = await getContestDetail(contestId);
+      return response.data;
+    },
+    staleTime: 1000 * 60 * 5,
+  });
+};
+
+export const useAddBookMarkContest = (contestId: number) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: () => postBookMarkedContest(contestId),
+    onSuccess: () => {
+      queryClient.refetchQueries({queryKey: ['contestDetail', contestId]});
+    },
+  });
+
+  return mutation;
+};
+
+export const useDeleteBookMarkContest = (contestId: number) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: () => deleteBookMarkedContest(contestId),
+    onSuccess: () => {
+      queryClient.refetchQueries({queryKey: ['contestDetail', contestId]});
+    },
+  });
+  return mutation;
+};
---
+++ src/services/api/contest/contestApi.ts
@@ -0,0 +1,77 @@
+import {
+  ContestDetailResponse,
+  ContestDetailResponseSchema,
+  ContestListResponseSchema,
+  PopularContestResponse,
+  PopularContestResponseSchema,
+} from '@/schemas/contests';
+import {axiosInstance} from '@/services/config/axios';
+import {API_ENDPOINTS} from '@/services/constant/endpoint';
+
+export const getPopularContest = async (): Promise<PopularContestResponse> => {
+  const {data} = await axiosInstance.get(API_ENDPOINTS.contest.popular);
+  return PopularContestResponseSchema.parse(data);
+};
+
+export const getContestDetail = async (
+  contestId: number
+): Promise<ContestDetailResponse> => {
+  const {data} = await axiosInstance.get(
+    API_ENDPOINTS.contest.detail(contestId)
+  );
+  return ContestDetailResponseSchema.parse(data);
+};
+
+export const getBookmarkedContestList = async ({
+  page = 0,
+  size = 20,
+  sortBy = '',
+}: {
+  page?: number;
+  size?: number;
+  sortBy?: string;
+}) => {
+  const {data} = await axiosInstance.get(API_ENDPOINTS.contest.bookmarked, {
+    params: {
+      pageable: {page, size, sort: [sortBy]},
+      sortBy,
+    },
+  });
+  return ContestListResponseSchema.parse(data);
+};
+
+export const deleteBookMarkedContest = async (contestId: number) => {
+  const {data} = await axiosInstance.delete(
+    API_ENDPOINTS.contest.bookmark(contestId)
+  );
+  return data;
+};
+
+export const postBookMarkedContest = async (contestId: number) => {
+  const {data} = await axiosInstance.post(
+    API_ENDPOINTS.contest.bookmark(contestId)
+  );
+  return data;
+};
+
+export const getContestList = async ({
+  page = 0,
+  size = 20,
+  sortBy = 'latest',
+}: {
+  page?: number;
+  size?: number;
+  sortBy?: string;
+}) => {
+  const {data} = await axiosInstance.get(API_ENDPOINTS.contest.contests, {
+    params: {
+      pageable: {page, size, sort: [sortBy]},
+      sortBy,
+    },
+  });
+  return ContestListResponseSchema.parse(data);
+};
+
+export const getContestSearchResult = async () => {
+  //TODO: 공모전 검색 결과 페이지 API & refactor
+};
