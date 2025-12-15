964ff80e8eb77bcfd054eb4768319c2edc231208
kimminna <kmina121777@naver.com>
2025-12-15 23:00:28 +0900

    refactor: remove unused contest search path (#62)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -6,7 +6,6 @@ export const ROUTES = {
   CONTEST: '/contest',
   IDEA: '/idea',
   REGISTER: '/register',
-  CONTEST_SEARCH: '/contest/search',
   SUPPORT: '/support',
   SUPPORT_INQUIRY: '/support/inquiry',
   SUPPORT_INQUIRY_NEW: '/support/inquiry/new',
