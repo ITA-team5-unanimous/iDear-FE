3072dc5289fd6071b80974f83b1f073f1bc7bd2e
kimminna <kmina121777@naver.com>
2025-12-15 23:07:01 +0900

    chore: add certificate mock data (#55)

---
+++ src/mocks/data/mockCertificateData.ts
@@ -0,0 +1,17 @@
+import {certificateType} from '@/schemas/certificate';
+
+export const mockCertificateData: certificateType[] = [
+  {
+    submission: '김민아',
+    title: '자동화 음료 쓰레기통',
+    submissionTime: '2025-11-20 15:04:32',
+    hash: 'ddd',
+    network: 'Polygon',
+    contractAddress: '0x123423',
+    transactionHash: '0x1344',
+    blockNumber: 123456,
+    onChainTimeStamp: '2025-11-20 15:04:32',
+    certificateId: 0,
+    certificateTime: '2025-11-13 15:23:14',
+  },
+];
