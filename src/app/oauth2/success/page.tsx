3e1b741056609c3412f857c44948684faa6d3df7
kimminna <kmina121777@naver.com>
2025-12-16 17:13:52 +0900

    chore: 임시 주석 처리 수정

--- src/app/oauth2/success/page.tsx
+++ src/app/oauth2/success/page.tsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 
 // import {Spinner} from '@/components/common/ui/Spinner';
 // import {ROUTES} from '@/constants/routes';
@@ -6,26 +6,23 @@
 // import {useRouter, useSearchParams} from 'next/navigation';
 // import {useEffect} from 'react';
 
-// export default function OAuth2SuccessPage() {
-//   const router = useRouter();
-//   const params = useSearchParams();
-//   const refresh = params.get('refresh');
-
-//   useEffect(() => {
-//     if (!refresh) return;
-
-//     (async () => {
-//       try {
-//         await reissueToken(refresh);
-//        개발모드 중 임시 주석 처리
-//         router.replace(ROUTES.COMPLETE);
-//         alert('로그인 성공. 개발 모드 중입니다. /complete 경로로 이동');
-//       } catch {
-//         alert('로그인에 실패했습니다. 다시 시도해 주세요.');
-//         router.replace(ROUTES.AUTH);
-//       }
-//     })();
-//   }, [refresh, router]);
-
-//   return <Spinner />;
-// }
+export default function OAuth2SuccessPage() {
+  // const router = useRouter();
+  // const params = useSearchParams();
+  // const refresh = params.get('refresh');
+  // useEffect(() => {
+  //   if (!refresh) return;
+  //   (async () => {
+  //     try {
+  //       await reissueToken(refresh);
+  //       router.replace(ROUTES.COMPLETE);
+  //       alert('로그인 성공. 개발 모드 중입니다. /complete 경로로 이동');
+  //     } catch {
+  //       alert('로그인에 실패했습니다. 다시 시도해 주세요.');
+  //       router.replace(ROUTES.AUTH);
+  //     }
+  //   })();
+  // }, [refresh, router]);
+  // return <Spinner />;
+  return <></>;
+}
