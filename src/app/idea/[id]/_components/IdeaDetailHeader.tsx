41c21c5c149189884dbc8093bb45c2a6514c5649
kimminna <kmina121777@naver.com>
2025-12-21 17:02:53 +0900

    feat: 아이디어 상세 헤더 화살표 버튼 클릭 시 공모전 상세로 이동 (#73)

--- src/app/idea/[id]/_components/IdeaDetailHeader.tsx
+++ src/app/idea/[id]/_components/IdeaDetailHeader.tsx
@@ -1,12 +1,21 @@
 import {BackButton} from '@/components/buttons/BackButton';
 import ChevronRight from '@/assets/chevrons/chevron-right.svg';
 import {Idea} from '@/schemas/idea';
+import {useRouter} from 'next/navigation';
+import {ROUTES} from '@/constants/routes';
 
 interface IdeaDetailHeaderProps {
   idea: Idea;
 }
 
 export const IdeaDetailHeader = ({idea}: IdeaDetailHeaderProps) => {
+  const router = useRouter();
+
+  const handleChevronRightClick = () => {
+    // TODO: 해당 아이디어의 공모전 상세 페이지로 이동
+    router.push(`${ROUTES.CONTEST}/${idea.id}`);
+  };
+
   return (
     <header className='mb-10 flex flex-col gap-[70px]'>
       <div className='flex flex-row'>
@@ -15,7 +24,10 @@ export const IdeaDetailHeader = ({idea}: IdeaDetailHeaderProps) => {
       </div>
       <div className='border-b-primary flex flex-row items-center justify-between border-b pb-4 text-[32px] font-bold'>
         <h1>{idea.title}</h1>
-        <button>
+        <button
+          type='button'
+          onClick={handleChevronRightClick}
+          aria-label='공모전 상세 페이지로 이동'>
           <ChevronRight />
         </button>
       </div>
