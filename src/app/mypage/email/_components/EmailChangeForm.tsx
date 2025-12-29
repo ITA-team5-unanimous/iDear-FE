d8b24db099f2668e1401c0223f28d21516bac55a
juhyung <njh1019@gmail.com>
2025-12-29 15:12:05 +0900

    feat: 이메일 변경 마이페이지 컴포넌트 구현 및 상수 정의 (#78)

---
+++ src/app/mypage/email/_components/EmailChangeForm.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import {NewEmailInput} from '@/app/mypage/email/_components/NewEmailInput';
+import GlobalButton from '@/components/buttons/GlobalButton';
+import {useState} from 'react';
+
+export const EmailChangeForm = () => {
+  const [currentEmail, setCurrentEmail] = useState<string>('abcdefghi@naver');
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+
+  const validateEmail = (value: string) =>
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+
+  const handleEmailChange = (value: string) => {
+    setEmail(value);
+
+    if (!value) {
+      setError('');
+    } else if (!validateEmail(value)) {
+      setError('이메일 주소가 올바르지 않아요');
+    } else {
+      setError('');
+    }
+  };
+
+  const handleClickCancel = () => {
+    setEmail('');
+    setError('');
+  };
+
+  const handleClickSave = () => {
+    if (!email || error) return;
+
+    setCurrentEmail(email);
+    setEmail('');
+  };
+
+  return (
+    <div className='flex flex-col gap-6'>
+      <div className='flex gap-3'>
+        <label className='text-gray text-xl'>현재 이메일</label>
+        <span className='text-xl'>{currentEmail}</span>
+      </div>
+
+      <div className='flex flex-col gap-12'>
+        <NewEmailInput
+          value={email}
+          error={error}
+          onChange={handleEmailChange}
+        />
+
+        <div className='flex justify-center gap-6'>
+          <GlobalButton
+            aria-label='취소하기 버튼'
+            text='취소하기'
+            variant='gray'
+            onClick={handleClickCancel}
+          />
+          <GlobalButton
+            aria-label='저장하기 버튼'
+            text='저장하기'
+            onClick={handleClickSave}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/mypage/email/_components/NewEmailInput.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import clsx from 'clsx';
+
+interface NewEmailInputProps {
+  value: string;
+  error?: string;
+  onChange: (value: string) => void;
+}
+
+export const NewEmailInput = ({value, error, onChange}: NewEmailInputProps) => {
+  return (
+    <div className='flex flex-col'>
+      <input
+        value={value}
+        placeholder='새 이메일'
+        onChange={(e) => onChange(e.target.value)}
+        className={clsx(
+          'bg-gray-3 placeholder:text-gray h-[65px] w-full rounded-lg px-6 text-xl outline-none',
+          error ? 'border-primary border-2' : 'border-gray border'
+        )}
+      />
+
+      {error && (
+        <span className='text-primary pt-3 text-xl'>
+          이메일 주소가 올바르지 않아요
+        </span>
+      )}
+    </div>
+  );
+};
