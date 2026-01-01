f89108c73964031934412520fe4ffbfb1c3a2c75
juhyung <njh1019@gmail.com>
2026-01-01 21:24:49 +0900

    feat: 아이디어 api 서비스 함수 구현 (#83)

---
+++ src/services/api/idea/ideaApi.ts
@@ -0,0 +1,57 @@
+import {
+  IdeaRegisterResponse,
+  IdeaRegisterResponseSchema,
+  IdeaSignatureResponse,
+  IdeaSignatureResponseSchema,
+  IdeaListResponse,
+  IdeaListResponseSchema,
+  IdeaDetailResponse,
+  IdeaDetailResponseSchema,
+} from '@/schemas/idea';
+import {axiosInstance} from '@/services/config/axios';
+import {API_ENDPOINTS} from '@/services/constant/endpoint';
+
+export const postIdeaRegister = async (
+  formData: FormData
+): Promise<IdeaRegisterResponse> => {
+  const {data} = await axiosInstance.post(
+    API_ENDPOINTS.idea.register,
+    formData,
+    {
+      headers: {
+        'Content-Type': undefined,
+      },
+    }
+  );
+
+  return IdeaRegisterResponseSchema.parse(data);
+};
+
+export const postIdeaSignatures = async (
+  ideaId: number,
+  signatures: {
+    ideaFileId: number;
+    userSignature: string;
+  }[]
+): Promise<IdeaSignatureResponse> => {
+  const {data} = await axiosInstance.post(
+    API_ENDPOINTS.idea.signatures(ideaId),
+    {signatures}
+  );
+
+  return IdeaSignatureResponseSchema.parse(data);
+};
+
+export const getIdeas = async (): Promise<IdeaListResponse> => {
+  const {data} = await axiosInstance.get(API_ENDPOINTS.idea.getIdeas);
+
+  return IdeaListResponseSchema.parse(data);
+};
+
+export const getIdeaDetail = async (
+  ideaId: number
+): Promise<IdeaDetailResponse> => {
+  const {data} = await axiosInstance.get(API_ENDPOINTS.idea.detail(ideaId));
+
+  return IdeaDetailResponseSchema.parse(data);
+};
