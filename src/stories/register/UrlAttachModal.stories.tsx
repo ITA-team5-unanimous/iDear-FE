c54e0eb9f5fb537d065c067f116ff71d913c24df
juhyung <njh1019@gmail.com>
2026-01-10 23:46:22 +0900

    refactor: update file structure and imports (#91)

--- src/stories/register/UrlAttachModal.stories.tsx
+++ src/stories/register/UrlAttachModal.stories.tsx
@@ -1,5 +1,5 @@
 import {Meta, StoryObj} from '@storybook/nextjs';
-import {UrlAttachModal} from '@/app/register/[id]/_components/file/UrlAttachModal';
+import {UrlAttachModal} from '@/components/common/file/UrlAttachModal';
 
 const meta: Meta<typeof UrlAttachModal> = {
   title: 'Components/Register/Url Attach Modal',
