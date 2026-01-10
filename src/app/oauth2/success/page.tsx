270a95b9c7f3ef79be8f98ff35b4fb089cad3a5b
juhyung <njh1019@gmail.com>
2026-01-10 19:06:07 +0900

    fix: 전체 주석으로 변경 (#99)

--- src/app/oauth2/success/page.tsx
+++ src/app/oauth2/success/page.tsx
@@ -1,31 +1,32 @@
 'use client';
 
-import {Spinner} from '@/components/common/ui/Spinner';
-import {ROUTES} from '@/constants/routes';
-import {ensurePublicKey, reissueToken} from '@/services/api/auth/authApi';
-import {useRouter, useSearchParams} from 'next/navigation';
-import {useEffect} from 'react';
+// 임시 주석 처리
+// import {Spinner} from '@/components/common/ui/Spinner';
+// import {ROUTES} from '@/constants/routes';
+// import {ensurePublicKey, reissueToken} from '@/services/api/auth/authApi';
+// import {useRouter, useSearchParams} from 'next/navigation';
+// import {useEffect} from 'react';
 
 export default function OAuth2SuccessPage() {
-  const router = useRouter();
-  const params = useSearchParams();
-  const refresh = params.get('refresh');
+  // const router = useRouter();
+  // const params = useSearchParams();
+  // const refresh = params.get('refresh');
 
-  useEffect(() => {
-    if (!refresh) return;
+  // useEffect(() => {
+  //   if (!refresh) return;
 
-    (async () => {
-      try {
-        await reissueToken(refresh);
-        await ensurePublicKey();
-        // 개발모드 중 임시 주석 처리
-        //router.replace(ROUTES.COMPLETE);
-      } catch {
-        alert('로그인에 실패했습니다. 다시 시도해 주세요.');
-        router.replace(ROUTES.AUTH);
-      }
-    })();
-  }, [refresh, router]);
+  //   (async () => {
+  //     try {
+  //       await reissueToken(refresh);
+  //       await ensurePublicKey();
+  //       router.replace(ROUTES.COMPLETE);
+  //     } catch {
+  //       alert('로그인에 실패했습니다. 다시 시도해 주세요.');
+  //       router.replace(ROUTES.AUTH);
+  //     }
+  //   })();
+  // }, [refresh, router]);
 
-  return <Spinner />;
+  // return <Spinner />;
+  return <></>;
 }
