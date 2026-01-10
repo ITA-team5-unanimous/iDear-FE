73e523ba9612c68137f80e77606cdf7b22ebc9f3
juhyung <njh1019@gmail.com>
2026-01-10 18:06:20 +0900

    feat: 최신 버전 시에만 수정 가능하도록 변경 (#91)

--- src/app/idea/[id]/_components/IdeaDetailMetaView.tsx
+++ src/app/idea/[id]/_components/IdeaDetailMetaView.tsx
@@ -5,35 +5,29 @@ import {AttachmentList} from '@/app/idea/[id]/_components/AttachmentList';
 import {IdeaVersionDetail} from '@/schemas/idea';
 import {useParams, useRouter} from 'next/navigation';
 import {ROUTES} from '@/constants/routes';
-import GlobalButton from '@/components/buttons/GlobalButton';
 import {useState} from 'react';
 import {ModalWrapper} from '@/components/common/wrappers/ModalWrapper';
-
-import {RegisterCompleteModal} from '@/components/common/modal/RegisterCompleteModal';
 import {GlobalAlertModal} from '@/components/common/modal/GlobalAlertModal';
 
 interface IdeaDetailMetaViewProps {
   version: IdeaVersionDetail;
-  isEditable?: boolean;
+  isLatestVersion: boolean;
 }
 
 export const IdeaDetailMetaView = ({
   version,
-  isEditable = false,
+  isLatestVersion,
 }: IdeaDetailMetaViewProps) => {
   const router = useRouter();
   const params = useParams();
 
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
-  const [isRegisterCompleteModalOpen, setIsRegisterCompleteModalOpen] =
-    useState<boolean>(false);
 
   const imageAttachments = version.images.map((img) => ({
     name: img.fileName,
     url: img.filePath,
   }));
 
-  // 2. 파일 데이터 가공
   const fileAttachments = version.files.map((file) => ({
     name: file.fileName,
     url: file.filePath,
@@ -63,26 +57,19 @@ export const IdeaDetailMetaView = ({
     setIsDeleteModalOpen(false);
   };
 
-  const handleSave = () => {
-    // TODO: 저장 API 호출
-
-    setIsRegisterCompleteModalOpen(true);
-  };
-
   return (
     <section className='border-gray flex w-full flex-col gap-12 rounded-[8px] border p-6'>
       <div className='flex flex-col gap-6'>
         <div className='flex flex-row items-center justify-between'>
           <strong className='text-2xl font-bold'>
-            ver.{version.versionNumber} : {version.shortDescription}
+            <span>ver.{version.versionNumber} :</span>
+            <span className='ml-3'>{version.shortDescription}</span>
           </strong>
           <div className='flex flex-row gap-4'>
-            {!isEditable && (
-              <>
-                <GlobalSmallButton text='수정하기' onClick={handleEditMode} />
-                <GlobalSmallButton text='삭제하기' onClick={handleDelete} />
-              </>
+            {isLatestVersion && (
+              <GlobalSmallButton text='수정하기' onClick={handleEditMode} />
             )}
+            <GlobalSmallButton text='삭제하기' onClick={handleDelete} />
           </div>
         </div>
         <p>
@@ -92,35 +79,22 @@ export const IdeaDetailMetaView = ({
 
       <div className='flex flex-col gap-6'>
         <strong className='text-2xl font-bold'>아이디어 설명</strong>
-        {isEditable ? (
-          <textarea
-            className='min-h-[200px] w-full'
-            defaultValue={version.description}
-          />
-        ) : (
-          <div className='overflow-auto'>{version.description}</div>
-        )}
+        <div className='overflow-auto'>{version.description}</div>
       </div>
       <div className='flex flex-col gap-6'>
         <strong className='text-2xl font-bold'>이미지</strong>
-        <AttachmentList
-          attachments={imageAttachments}
-          isEditable={isEditable}
-        />
+        <AttachmentList attachments={imageAttachments} />
       </div>
 
       <div className='flex flex-col gap-6'>
         <strong className='text-2xl font-bold'>파일</strong>
-        <AttachmentList attachments={fileAttachments} isEditable={isEditable} />
+        <AttachmentList attachments={fileAttachments} />
+        <span className='text-primary text-base'>
+          ※ 문서 파일 변경시에만 새로운 버전이 생성됩니다.
+        </span>
         {linkAttachments.length > 0 && (
-          <AttachmentList
-            attachments={linkAttachments}
-            isEditable={isEditable}
-          />
+          <AttachmentList attachments={linkAttachments} />
         )}
-      </div>
-      <div className='flex w-full justify-center'>
-        {isEditable && <GlobalButton text='저장하기' onClick={handleSave} />}
       </div>
 
       {isDeleteModalOpen && (
@@ -133,19 +107,6 @@ export const IdeaDetailMetaView = ({
             content='한 번 삭제하면 다시 되돌릴 수 없어요.'
             onClose={handleConfirmDelete}
             onContinue={() => setIsDeleteModalOpen(false)}
-          />
-        </ModalWrapper>
-      )}
-
-      {isRegisterCompleteModalOpen && (
-        <ModalWrapper
-          isOpen={isRegisterCompleteModalOpen}
-          onClose={() => setIsRegisterCompleteModalOpen(false)}>
-          <RegisterCompleteModal
-            onClose={() => {
-              setIsRegisterCompleteModalOpen(false);
-              router.push(`${ROUTES.IDEA}/${params.id}`);
-            }}
           />
         </ModalWrapper>
       )}
