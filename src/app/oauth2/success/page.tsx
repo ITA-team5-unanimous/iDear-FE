7fe8b2903d3acbf722cbbeea91878500f1870dce
juhyung <njh1019@gmail.com>
2026-01-10 18:24:19 +0900

    chore: 로그인시 리다이렉트 설정 임시 OFF (#99)

--- src/app/oauth2/success/page.tsx
+++ src/app/oauth2/success/page.tsx
@@ -1,30 +1,30 @@
 'use client';
 
-import {Spinner} from '@/components/common/ui/Spinner';
-import {ROUTES} from '@/constants/routes';
-import {ensurePublicKey, reissueToken} from '@/services/api/auth/authApi';
-import {useRouter, useSearchParams} from 'next/navigation';
-import {useEffect} from 'react';
+// import {Spinner} from '@/components/common/ui/Spinner';
+// import {ROUTES} from '@/constants/routes';
+// import {ensurePublicKey, reissueToken} from '@/services/api/auth/authApi';
+// import {useRouter, useSearchParams} from 'next/navigation';
+// import {useEffect} from 'react';
 
-export default function OAuth2SuccessPage() {
-  const router = useRouter();
-  const params = useSearchParams();
-  const refresh = params.get('refresh');
+// export default function OAuth2SuccessPage() {
+//   const router = useRouter();
+//   const params = useSearchParams();
+//   const refresh = params.get('refresh');
 
-  useEffect(() => {
-    if (!refresh) return;
+//   useEffect(() => {
+//     if (!refresh) return;
 
-    (async () => {
-      try {
-        await reissueToken(refresh);
-        await ensurePublicKey();
-        router.replace(ROUTES.COMPLETE);
-      } catch {
-        alert('로그인에 실패했습니다. 다시 시도해 주세요.');
-        router.replace(ROUTES.AUTH);
-      }
-    })();
-  }, [refresh, router]);
+//     (async () => {
+//       try {
+//         await reissueToken(refresh);
+//         await ensurePublicKey();
+//         router.replace(ROUTES.COMPLETE);
+//       } catch {
+//         alert('로그인에 실패했습니다. 다시 시도해 주세요.');
+//         router.replace(ROUTES.AUTH);
+//       }
+//     })();
+//   }, [refresh, router]);
 
-  return <Spinner />;
-}
+//   return <Spinner />;
+// }
